b21b9c31528b0c24a5d9d1f9663bd974356efa90
T-revin <89531653+T-revin@users.noreply.github.com>
2025-11-01 20:08:53 +0000

    Added an arrow icon with text "Scroll" to inform uses that they can scroll down for more content on Hero section page

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,5 @@
+import {ArrowDown} from 'lucide-react';
+
 export const HeroSection = () => {
   return (
     <section
@@ -23,12 +25,17 @@ export const HeroSection = () => {
             and knowledge to contribute to innovative projects.
           </p>
 
-          <div>
+          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
             <a href="#project" className="cosmic-button">
               View My Work
             </a>
           </div>
         </div>
+      </div>
+
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+        <span> Scroll </span>
+        <ArrowDown className="h-5 w-5 text-primary" />
       </div>
     </section>
   );
