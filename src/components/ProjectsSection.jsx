4b580d278fa7579711163c4fd58243bc8df13fdd
T-revin <89531653+T-revin@users.noreply.github.com>
2025-11-02 14:43:36 +0000

    New sections "ProjectSection.jsx" file created in components folder and imported into "Home.jsx" Added images for 3 projects in new "projects" subfolder in "public" folder. Array of projects created in ProjectsSection.jsx

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,41 @@
+const projects = [
+  {
+    id: 1,
+    title: 'Group Allocation Web App',
+    description:
+      'A web application that automates the process of allocating students into groups based on various criteria such as preferences and availability. Built using React for the frontend, connected to a SQL Database using backend API built with Express.js.',
+    image: '/projects/CSEConnect.jpg',
+    tags: [
+      'React',
+      'Express.js',
+      'SQL Database',
+      'Web Development',
+      'Group Project',
+    ],
+    demoURL: '#',
+    gitHubUrl: '#',
+  },
+  {
+    id: 2,
+    title: 'Power BI Dashboard for Student Outcomes Analysis',
+    description:
+      'A Power BI dashboard that visualises student outcomes data to identify trends and insights. The dashboard includes interactive charts and graphs to help stakeholders make data-driven decisions.',
+    image: '/projects/DataManagement.jpg',
+    tags: ['Power BI', 'Data Visualisation', 'Data Analysis'],
+    demoURL: '#',
+    gitHubUrl: '#',
+  },
+  {
+    id: 3,
+    title: 'Excel Based Student Performance Tracker',
+    description:
+      'An Excel workbook designed to track and analyse student performance over time. The tracker focuses on the key metric of student grades, using formulas and visual cues to assist day-to-day monitoring and long-term analysis.',
+    tags: ['Microsoft Excel', 'Data Analysis', 'Data Management'],
+    demoURL: '#',
+    gitHubUrl: '#',
+  },
+];
+
+export const ProjectsSection = () => {
+  return <section></section>;
+};
